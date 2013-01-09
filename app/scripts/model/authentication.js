f70d0214c30a49d9b5e6b119b370bd14f9d872b7
Patrick Williams <pwmckenna@gmail.com>
2013-01-08 16:00:47 -0800

    not connecting immediately

--- app/scripts/model/authentication.js
+++ app/scripts/model/authentication.js
@@ -8,27 +8,30 @@ define([
         initialize: function() {
             this.feeds = new Firebase('https://feedmixalot.firebaseIO.com/feeds');
             this.auth = new FirebaseAuthClient(this.feeds);
-            this.getLoginStatus();
+            this.set('status', 'disconnected');
         },
         getLoginStatus: function() {
+            console.log('getLoginStatus');
             FB.getLoginStatus(_.bind(this.onLoginStatus, this));
         },
         onLoginStatus: function(response) {
+            console.log('onLoginStatus', response);
             this.set('status', response.status);
             this.set(response.authResponse);
         },
         login: function() {
-            this.auth.login('facebook', _.bind(function(error, token, user) {
-                console.log(error, token, user);
-                if (!error) {
-                    // You can now do Firebase operations as an authenticated user...
-                    console.log('User ID: ' + user.id); // '1234'
-                    console.log('Provider: ' + user.provider); // 'facebook'
-                } else {
-                    console.log(error);
-                }
-                this.getLoginStatus();
-            }, this));
+            this.auth.login('facebook', _.bind(this.onLogin, this));
+        },
+        onLogin: function(error, token, user) {
+            console.log('onLogin', error, token, user);
+            if (!error) {
+                // You can now do Firebase operations as an authenticated user...
+                console.log('User ID: ' + user.id); // '1234'
+                console.log('Provider: ' + user.provider); // 'facebook'
+            } else {
+                console.log(error);
+            }
+            this.getLoginStatus();
         }
     });
     return AuthenticationModel;
