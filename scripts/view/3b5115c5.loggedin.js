c04ce26ce9d6240ede450e417122051434c592bc
Patrick Williams <pwmckenna@gmail.com>
2013-01-14 11:32:29 -0800

    updating site

---
+++ scripts/view/3b5115c5.loggedin.js
@@ -0,0 +1,53 @@
+define([
+    'jquery',
+    'underscore',
+    './view',
+    './feed'
+], function($, _, View, FeedView) {
+    'use strict';
+    var LoggedInView = View.extend({
+        events: {
+            'click .addFeed': 'onAddFeed'
+        },
+        initialize: function() {
+            _.bindAll(this, 'onFeedAdded', 'onFeedRemoved');
+            this.template = _.template($('#logged_in_template').html());
+            this.model.on('change:status', this.onStatus, this);
+            this.views = [];
+        },
+        onFeedAdded: function(feedChildSnapshot) {
+            var view = new FeedView({
+                model: feedChildSnapshot
+            });
+            this.views[feedChildSnapshot.name()] = view;
+            this.$('.feeds').append(view.render().el);
+        },
+        onFeedRemoved: function(feedChildSnapshot) {
+            this.views[feedChildSnapshot.name()].remove();
+            delete this.views[feedChildSnapshot.name()];
+        },
+        onAddFeed: function(ev) {
+            this.model.feeds.push({
+                name: 'new feed',
+                facebook_id: this.model.get('user').id
+            });
+        },
+        onStatus: function() {
+            if(this.model.get('status')) {
+                this.model.feeds.on('child_added', this.onFeedAdded);
+                this.model.feeds.on('child_removed', this.onFeedRemoved);
+            }
+            this.render();
+        },
+        render: function() {
+            this.$el.html(this.template());
+            if(this.model.get('status')) {
+                this.$el.show();
+            } else {
+                this.$el.hide();
+            }
+            return this;
+        }
+    });
+    return LoggedInView;
+});
