bc3b44e5d95b54bf414eefece3e7c276d29a4c5c
Patrick Williams <pwmckenna@gmail.com>
2013-01-31 16:06:13 -0800

    storing authentication token in firebase so the server can use it for todium api calls

--- app/scripts/model/authentication.js
+++ app/scripts/model/authentication.js
@@ -19,6 +19,13 @@ define([
             console.log('onLogin', error, user);
             if (!error) {
                 this.set('user', user);
+
+                if(user) {
+                    //store the access token so that we can add rss feed links to todium on behalf of the user
+                    this.firebase.child('users').child(user.id).update({
+                        'token': user.firebaseAuthToken
+                    });
+                }
             } else {
                 console.log(error);
             }
